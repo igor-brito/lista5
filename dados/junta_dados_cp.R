6afa3223b937777542531f74c8ab8d3966a2ceae
Igor Brito <igor.br03@gmail.com>
2022-09-16 09:20:28 -0300

    Junta todas as bases de dados em uma só para a lista5 e o trabalho final.

--- dados/junta_dados_cp.R
+++ dados/junta_dados_cp.R
@@ -1,10 +1,19 @@
 #Autor: Igor Brito
-#Data: 12/09/22
+#Data: 16/09/22
 
 #o objetivo deste código é criar uma base com os dados do tamanho do 
-#centro presidencial brasileiro entre 1985 e 2022. Para tanto, irei 
-#juntar os dados coletados por Inácio e Llanos (2015), que cobrem o
+#centro presidencial e da composição dos gabinetes brasileiros entre
+# 1985 e 2016 para ser utilizada na lista 5 e no trabalho final da
+#disciplina de Métodos Quantitativos em Ciência Política.
+
+#Em relação aos dados do tamanho do centro presidencial, irei juntar
+#os dados coletados por Inácio e Llanos (2015), que cobrem o
 #período de 1985 a 2010, com os dados que eu coletei (2003-2022).
+
+#No que se refere aos dados de composição do gabinete, utilizarei os
+#dados apresentados por Amorim Neto (2019) e disponibilizados diretamente
+#no texto do trabalho. Esses dados já foram extraídos e disponibilizados
+#em formato csv em um momento anterior.
 
 #Obs: os dados de Inácio e Llanos (2015) possuem granularidade
 #anual, ou seja, os dados revelam o tamanho do centro presidencial
@@ -14,6 +23,17 @@
 #granularidade diária, ou seja, os dados podem indincar qual o tamanho
 #do centro presidencial em cada dia. Como irei juntar essas bases, acabou
 #que os dados finais terão granularidade anual.
+
+#Referências:
+#AMORIM NETO, Octavio. Cabinets and coalitional presidentialism.
+#In: AMES, Barry (ed.). Routledge Handbook of Brazilian
+#Politics. New York: Routledge, 2019. p. 293-312.
+
+#INÁCIO, Magna; LLANOS, Mariana. The institutional presidency
+#from a comparative perspective: Argentina and Brazil since the
+#1980s. Brazilian Political Science Review, São Paulo, v. 9, n. 1,
+#p. 39-64, 2015. 
+
 
 
 #### Procedimentos iniciais
@@ -32,9 +52,11 @@ library(magrittr)
 library(zoo)
 library(lubridate)
 
+#### Dados de tamanho do centro presidencial ####
 #Carrega dados
 dados_inacio <- read_xlsx("INACIO_LLANOS_DATABASE.xlsx")
 dados_igor <- read_csv2("dados_pe_brasil.csv")
+
 
 #Existe um problema de codificação que não consegui resolver neste momneto.
 #Como isso poderia demandar muito tempo de pesquisa e esse problema não afeta
@@ -84,6 +106,30 @@ dados_anuais %<>%
   bind_rows(dados_inacio) %>% 
   arrange(ano)
 
-#Exporta os dados
-write.csv2(dados_anuais, "cp_Brasil_anual.csv", na = "", row.names = F)
 
+
+#### Dados de composição do gabinete ####
+#Importa os dados de tamanho e composição dos gabinetes
+gabinetes_brasil <- read_csv("gabinetes_Brasil_Amorim_mensal.csv")
+
+#A referência para registro das informações do ano é o seu primeiro dia (01/jan)
+#Contudo, essa definição excluiria o ano de 1985, visto que o governo Sarney 1
+#começou apenas em março. Portanto, fiz a modificação abaixo apenas para garantir
+#que o ano de 1985 fosse incluído na análise.
+gabinetes_brasil[1,1] <- as.Date("1985-01-01")
+
+#Pega o gabinete em cada ano (feito o ajuste acima para o ano de 1985).
+gabinetes_brasil %<>%
+  filter(month(inicio) == 1 & day(inicio) == 1) %>% 
+  mutate(ano = year(inicio)) %>% 
+  select(ano, gabinete, qtd_part, partidos)
+
+
+
+#### Junta os dados de tamanho do centro presidencial com os dados de gabinete ####
+dados_trab_final <- 
+  left_join(dados_anuais, gabinetes_brasil, by = "ano") %>% 
+  filter(!is.na(gabinete))
+
+#Exporta os dados
+write.csv2(dados_trab_final, "dados_trab_final.csv", na = "", row.names = F)
